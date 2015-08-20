e54d6bd058423f0277cdd763fe9369a0e805409e
Josh Medeski <josh@medology.com>
2015-08-20 14:50:21 -0500

    init gh-page!

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
+
+var sassPaths = [
+  'bower_components/foundation/scss',
+  'bower_components/fontawesome/scss',
+  'bower_components/foundation-font-awesome-buttons/scss'
+];
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths,
+      errLogToConsole: true
+    }))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
