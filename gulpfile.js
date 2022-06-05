2e17f538b5ae8f4d6e43840ec9931ecee760794f
nanawel <nanawel@gmail.com>
2022-06-05 12:06:15 +0200

    Upgrade to Font Awesome 6 and Node 12+

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,39 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var $    = require('gulp-load-plugins')();
+var $           = require('gulp-load-plugins')();
+var sass        = require('gulp-sass')(require('sass'));
 
 var sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/@fortawesome/fontawesome-free/scss',
+  'node_modules/@sass-collective',
   'src'
 ];
 
-gulp.task('sass', function () {
+var sassTask = function () {
   return gulp.src(['preview/*.scss', 'src/*.scss'])
-    .pipe($.sass({
-      includePaths: sassPaths
-    })
-    .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
-});
+      .pipe(sass({
+        includePaths: sassPaths
+      })
+          .on('error', sass.logError))
+      .pipe($.autoprefixer())
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.stream());
+};
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('sass', sassTask);
+
+gulp.task('watch', function() {
   browserSync.init({
     server: { baseDir: "./" }
   });
 
-  gulp.watch('**/*.scss', ['sass']);
+  gulp.watch('**/*.scss', sassTask);
   gulp.watch("**/*.html").on('change', browserSync.reload);
 });
 
+var serveTask = gulp.series('sass', 'watch');
 
-gulp.task('default', ['sass', 'serve'], function () { });
+gulp.task('serve', serveTask);
+
+gulp.task('default', serveTask);
